a2e57bbb300c616bd583529a9b1aeacd0c8f380d
LekenSuvar <87902339+LekenSuvar@users.noreply.github.com>
2022-07-19 21:27:33 +0400

    To Do List 1.2

--- js/check.js
+++ js/check.js
@@ -2,30 +2,28 @@
 const checkTask = document.querySelector('.main__task'),
     editChange = document.querySelectorAll('.edit')
 
-console.log(editChange.length)
-
 checkTask.addEventListener('click', (event) => {
     if (event.target.className == 'check') {
         event.target.classList.add('done')
         event.target.parentNode.classList.add('border')
     } 
-    else if (event.target.className == 'edit'){
+    else if (event.target.className == 'edit' || event.target.className == 'edit process'){
         if(event.target.previousSibling.hasAttribute('readonly') === true){
             event.target.previousSibling.removeAttribute('readonly')
             event.target.previousSibling.focus()
-            event.target.style.background = 'url(../img/confirm.svg) no-repeat center top / cover'
-        }
+            event.target.classList.add('process')
+        }   
         else{
+            event.target.classList.remove('process')
             event.target.previousSibling.setAttribute('readonly', false)
-            event.target.style.background = 'url(../img/edit.svg) no-repeat center top / cover'
         }
     }
     else if (event.target.className == 'delete'){
         event.target.parentNode.style.display = 'none'
     }
     else{
+        event.target.classList.remove('process')
         event.target.classList.remove('done')   
         event.target.parentNode.classList.remove('border')
-        event.target.previousSibling.setAttribute('readonly', false)
     }
 }, false)
